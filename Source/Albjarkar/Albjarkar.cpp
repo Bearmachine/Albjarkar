c41a2481325fcf8bb934f37cf643e8db3bbc538c
Bearmachine <wahlberg.bjorn@hotmail.com>
2017-06-18 20:54:44 +0200

    Initial commit

---
+++ Source/Albjarkar/Albjarkar.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Albjarkar.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Albjarkar, "Albjarkar" );
